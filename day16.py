a15aa3580cfc84b9a2d40b2fdd24461a871eefe1
kangmunju <ikmj0871@gmail.com>
2023-02-06 23:54:59 +0900

    mission ch5 ex2

--- day16.py
+++ day16.py
@@ -1,105 +1,68 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.link = None
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE-1:
+        return True
+    return False
 
 
-def print_nodes(start):
-    current = start
-    if current == None :
+def is_stack_empty():
+    global SIZE, stack, top
+    if top == -1:
+        return True
+    return False
+
+
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
+        print("Stack is FULL!")
         return
-    print(current.data, end=' ')
-    while current.link != None:
-        current = current.link
-        print(current.data, end=' ')
-    print()
+    top = top + 1
+    stack[top] = data
 
 
-def insert_nodes(find_data, insert_data):
-    global head, current, pre
-    if head.data == find_data:
-        node = Node(insert_data)
-        node.link = head
-        head = node
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is EMPTY~")
         return
-
-    current = head
-    while current.link != None:
-        pre = current
-        current = current.link
-        if current.data == find_data:
-            node = Node(insert_data)
-            node.link = current
-            pre.link = node
-            return
-
-    node = Node(insert_data)
-    current.link = node
+    temp = stack[top]
+    stack[top] = None
+    top = top - 1
+    return temp
 
 
-def delete_nodes(delete_data):
-    global head, current, pre
-
-    if head.data == delete_data:
-        print('첫 번째 노드 삭제 완료')
-        current = head
-        head = head.link
-        del current
-        return
-
-    current = head
-    while current.link != None:
-        pre = current
-        current = current.link
-        if current.data == delete_data:
-            print('중간 노드 삭제 완료')
-            pre.link = current.link
-            del current
-            return
-
-    print('삭제할 노드를 찾지 못함')
+def peek():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is EMPTY~")
+        return None
+    return stack[top]
 
 
-def find_nodes(find_data):
-    global head, current, pre
-
-    current = head
-    if current.data == find_data:
-        return current
-
-    while current.link != None:
-        current = current.link
-        if current.data == find_data:
-            return current
-
-    return Node(None)
-
-
-head, current, pre = None, None, None
-data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+SIZE = int(input("Stack Size : "))
+stack = [None for _ in range(SIZE)]
+top = -1
 
 
 if __name__ == "__main__":
-    node = Node(data_array[0])
-    head = node
+    while True:
+        menu = input("Insert(I)/Extract(E)/Verify(V)/eXit(X) : ")
+        if menu == 'X' or menu == 'x':
+            break
+        elif menu == 'I' or menu == 'i':
+            data = input("Input Data : ")
+            push(data)
+            print("Stack Status : ", stack)
+        elif menu == 'E' or menu == 'e':
+            data = pop()
+            print("Extrated Data : ", data)
+            print("Stack Status : ", stack)
+        elif menu == 'V' or menu == 'v':
+            data = peek()
+            print("Check Data : ", data)
+            print("Stack Status : ", stack)
+        else:
+            print("Input mismatch")
 
-    for data in data_array[1:]:
-        pre = node
-        node = Node(data)
-        pre.link = node
-
-    print_nodes(head)
-    insert_nodes("피카츄", "잠만보")
-    print_nodes(head)
-    insert_nodes("파이리", "어니부기")
-    print_nodes(head)
-    insert_nodes("성윤모", "거북왕")
-    print_nodes(head)
-    delete_nodes("잠만보")
-    print_nodes(head)
-    delete_nodes("어니부기")
-    print_nodes(head)
-    delete_nodes("강찬석")
-    print_nodes(head)
-    print(find_nodes("파이리").data)
-    print(find_nodes("박민석").data)
+    print("Program End")
