27672f539900972090c6d23d12b2e63bb7dc5816
kangmunju <ikmj0871@gmail.com>
2023-02-08 00:07:19 +0900

    ìãìch 8 ãex1

--- day17mission.py
+++ day17mission.py
@@ -1,58 +1,55 @@
-#ch 7 ex 2
+import random
+## 함수 선언 부분 ##
+class TreeNode() :
+	def __init__ (self) :
+		self.left = None
+		self.data = None
+		self.right = None
 
-def isQueuefull():
-	global SIZE, queue, front, rear
-	if ((rear+1) % SIZE == front):
-		return True
-	else:
-		return False
+## 전역 변수 선언 부분 ##
+memory = []
+root = None
+dataAry = ['바나나맛우유', '레쓰비캔커피', '츄파춥스', '도시락',  '삼다수', '코카콜라' ,'삼각김밥']
+sellAry = [ random.choice(dataAry) for _ in  range(20)]
 
-def isQueueEmpty():
-	global SIZE, queue, front, rear
-	if (front == rear):
-		return True
-	else:
-		return False
-def enQueue(data):
-	global SIZE, queue, front, rear
-	if (isQueuefull() == True):
-		print("대기콜이 꽉 찼습니다")
+print('오늘 판매된 물건(중복O) -->', sellAry)
+
+## 메인 코드 부분 ##
+node = TreeNode()
+node.data = sellAry[0]
+root = node
+memory.append(node)
+
+for name in sellAry[1:] :
+
+	node = TreeNode()
+	node.data = name
+
+	current = root
+	while True :
+		if name == current.data :
+			break
+		if name < current.data :
+			if current.left == None :
+				current.left = node
+				memory.append(node)
+				break
+			current = current.left
+		else :
+			if current.right == None :
+				current.right = node
+				memory.append(node)
+				break
+			current = current.right
+
+print("이진 탐색 트리 구성 완료!")
+
+def preorder(node) :
+	if node == None :
 		return
-	rear = (rear+1) % SIZE
-	queue[rear] = data
+	print(node.data, end = ' ')
+	preorder(node.left)
+	preorder(node.right)
 
-def deQueue():
-	global SIZE, queue, front, rear
-
-	if (isQueueEmpty() == True):
-		print("대기콜이 비었습니다")
-		return None
-	front = (front+1) % SIZE
-	data = queue[front]
-	queue[front] = None
-	return data
-
-def calctime():
-	global SIZE, queue, front, rear
-	time = 0
-	for i in range((front+1)%SIZE, (rear+1)%SIZE):
-		time += queue[i][1]
-	return time
-
-#전역 변수
-SIZE = 6
-queue = [None for _ in range(SIZE)]
-front = rear = 0
-
-
-#메인 함수
-if __name__ == "__main__":
-	waitcall = [('사용',9),('고장',3),('환불',4),('환불',4),('고장',3)]
-	for call in waitcall:
-		print(f'귀하의 예상 대기시간은 {calctime()}분입니다.')
-		print(f'현재 대기 콜-->{queue}')
-		enQueue(call)
-		print()
-
-	print("최종 대기 콜 --> ",queue)
-	print("프로그램 종료!")
+print('오늘 판매된 종류(중복X)--> ', end = ' ')
+preorder(root)
